98f70f78de48e33388226e942d52dd186e55d55a
DanielMarella <marella.daniel96@gmail.com>
2023-05-18 16:40:01 +0200

    first snack done

--- jsnack-1/js/myscript.js
+++ jsnack-1/js/myscript.js
@@ -7,3 +7,18 @@ Il programma stampa la somma di tutti i numeri inseriti.
 
 **/
 
+/* Creo un ciclo for che si ripeta per 10 volte
+    Creo la variabile userNumber e chiedo all'utente 10 volte un numero
+        Creo un ciclo all'interno del for che mi vada a scrivere il numero appena inserito e mi  vada a fare la somma di tutti i numeri che inserisco
+            esco dal ciclo
+*/
+
+let sumNumber = 0;
+for (i = 0; i < 10; i++){
+    const userNumber = parseInt(prompt('Scrivi un numero'));
+        sumNumber += userNumber;
+        console.log(sumNumber);
+}
+console.log('la somma finale è: ' + sumNumber);
+
+
