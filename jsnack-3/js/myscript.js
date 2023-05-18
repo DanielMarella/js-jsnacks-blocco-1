20640dcfe62939e913f4e09b4c279a150209c84b
DanielMarella <marella.daniel96@gmail.com>
2023-05-18 17:38:11 +0200

    done snack-3

--- jsnack-3/js/myscript.js
+++ jsnack-3/js/myscript.js
@@ -7,3 +7,13 @@ Chiedi per 6 volte all’utente di inserire un numero,  se è dispari inseriscil
 
 **/
 
+const oddNumber = [];
+
+for( let i = 0 ; i < 6; i++){
+    let userNumber= parseInt(prompt('Inserisci un numero qualsiasi'));
+    if(userNumber % 2 === 1){
+        oddNumber.push(userNumber);
+    }
+    
+}
+console.log(oddNumber);
