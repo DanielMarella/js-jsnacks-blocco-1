7a540e5e0def74b429ea95822d5eff8148a46ce0
DanielMarella <marella.daniel96@gmail.com>
2023-05-18 17:17:25 +0200

    snack 2 done

--- jsnack-2/js/myscript.js
+++ jsnack-2/js/myscript.js
@@ -6,3 +6,20 @@ In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, c
 
 **/
 
+const nameInvited = ['franco', 'ugo', 'ughetto', 'bepi', 'beppe', 'ubaldo', 'gaudi', 'claudio', 'kaio', 'beppino', 'giuseppina', 'zeudi'];
+
+const userName = prompt('inserisci il tuo nome');
+let NameFound = false;
+
+for (let i= 0; i < nameInvited.length; i++){
+    if(nameInvited[i] === userName){
+        NameFound = true;
+    }
+}
+
+if (NameFound) {
+    console.log("la persona può partecipare alla festa");
+}
+    else {
+    console.log("la persona non può partecipare alla festa");
+}
