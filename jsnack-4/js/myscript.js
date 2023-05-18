8e374836fad0d89b39e0e95c67631dab94072cdc
DanielMarella <marella.daniel96@gmail.com>
2023-05-18 18:55:31 +0200

    done snack 4

--- jsnack-4/js/myscript.js
+++ jsnack-4/js/myscript.js
@@ -7,3 +7,20 @@ e calcola la somma di tutte le cifre che compongono il numero.
 
 **/
 
+
+const userNumber = (prompt('Digita un numero a 4 cifre'));
+let sumNumber = 0;
+const numberArray=[];
+
+for (let i = 0; i < userNumber.length; i++){
+    
+    if (i < userNumber.length){
+        numberArray.push(parseInt(userNumber[i]))
+        console.log(numberArray);
+        sumNumber += numberArray[i];
+        console.log(sumNumber)
+    }
+}
+
+
+
